073ee0176b5bf8cd10d7aefac1e56736ba7e3e49
drwn-srrws <110095121+drwn-srrws@users.noreply.github.com>
2022-09-28 22:02:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This task done by Labunets Yaroslav SE-19-1/9");
         Date date = new Date();
-        System.out.println("Current date" +date);
+        System.out.println("Current date" +date);//date
     }
 }
