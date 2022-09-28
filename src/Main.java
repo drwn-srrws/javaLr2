8bbcb3688eae2a21b8e31b64cd4e3380cf08d8b4
drwn-srrws <afoninayou@gmail.com>
2022-09-28 22:00:11 +0300

    console date

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This task done by Labunets Yaroslav SE-19-1/9");
+        Date date = new Date();
+        System.out.println("Current date" +date);
     }
 }
