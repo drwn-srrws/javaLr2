f97a5e4458dfba89d5555f9a09afe271b428edec
drwn-srrws <afoninayou@gmail.com>
2022-09-28 21:47:00 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This task done by Labunets Yaroslav SE-19-1/9");
+    }
+}
